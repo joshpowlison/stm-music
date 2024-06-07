c9171042c338a282062782bdeab9aaf50c070325
Josh Powlison <joshuapowlison@gmail.com>
2024-06-07 19:45:13 -0400

    Updated functionality: play songs, play albums, shuffle all songs (true shuffle, not choosing a random song on end), shuffle an album, queue songs, queue albums, and skip songs

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const moduleFunctions = {
 	"loadSettings": loadSettings,
-	"play": musicPlay,
-	"playRandom": musicPlayRandom,
-	"pause": musicPause,
-	"stop": musicStop,
-	"shuffle": musicShuffle,
-	"logAllOptions": logAllOptions
+	"play": play,
+	"playSong": playSong,
+	"pause": pause,
+	"stop": stop,
+	"shuffle": shuffle,
+	"skip": skip,
+	"queueSong": queueSong,
+	"queueAlbum": queueAlbum,
+	"shuffleAlbum": shuffleAlbum,
+	"logAllOptions": logAllOptions,
 };
 
 module.LoadModule(moduleFunctions);
@@ -15,109 +19,170 @@ module.LoadModule(moduleFunctions);
 ///////////////////
 
 const player = document.querySelector('audio');
+const SUPPORTED_EXTENSIONS = [ 'mp3', 'wav', 'flac', 'aiff' ];
 
 ///////////////////
 //// VARIABLES ////
 ///////////////////
 
-let isShuffling = false;
 let items = [];
 let folderName = '../../userData/Music/';
+
+let queue = [];
 
 ///////////////////
 //// FUNCTIONS ////
 ///////////////////
+
 async function loadSettings(name, event) {
-	items = Utility.getAllPaths(module.globalSettings.fileStructure.userData.Music, [ 'mp3', 'wav', 'flac', 'aiff' ]);
+	items = Utility.getAllPaths(module.globalSettings.fileStructure.userData.Music, SUPPORTED_EXTENSIONS);
 }
 
-async function musicPlay(name, event) {
-	// If we've moved to a new track, go there
-	if (event !== null)
-	{
-		let item = Utility.getMatchingFileInList(items, event);
-		
-		// If no match, log an error
-		if (item === null)
-		{
-			module.F('Console.LogError', 'No Music track named "' + JSON.stringify(event) + '" found.');
-			return;
-		}
-
-		player.currentTime	= 0;
-		player.src = folderName + item;
+async function tryGetItem(name) {
+	let item = null;
+	
+	if (name !== null) {
+		item = Utility.getMatchingFileInList(items, name);
 	}
-
-	player.play();
-
-	// Mute game
-	// TODO: Pull this out. This is a separate piece of logic,
-	//  Music shouldn't know about it. AND it's my personal setup,
-	//  unrelated to many people, could cause random surprises.
-	module.F('OBS.MuteAudioSource', 'Zoom HD60S');
+	
+	if (item === null) {
+		module.F('Console.LogError', 'No Music track named "' + JSON.stringify(name) + '" found.');
+	}
+	
+	return item;
 }
 
-async function musicPlayRandom() {
-	let item = Utility.getRandomItem(items);
-
-	player.currentTime	= 0;
-	player.src = folderName + item;
-	player.play();
+async function tryGetAlbum(albumName) {
+	let albumItems = null;
+	
+	if(albumName in module.globalSettings.fileStructure.userData.Music){
+		albumItems = Utility.getAllPaths(module.globalSettings.fileStructure.userData.Music[albumName], SUPPORTED_EXTENSIONS);
+	}
+	
+	if (albumItems === null) {
+		module.F('Console.LogError', 'No Music album named "' + JSON.stringify(albumName) + '" found.');
+	}
+	
+	return albumItems;
 }
 
-async function musicPause(name, event) {
+async function play(name, event) {
+	if(!player.paused) {
+		module.F('Console.LogError', 'Music is currently playing.');
+		return;
+	}
+	
+	if(player.src === null) {
+		module.F('Console.LogError', 'No song is currently queued.');
+		return;
+	}
+	
+	await player.play();
+}
+
+async function playSong(name, event) {
+	let item = await tryGetItem(event);
+	if (item === null) {
+		return;
+	}
+	
+	queue = [event];
+	await tryPlayNextInQueue();
+
+	await player.play();
+}
+
+async function pause(name, event) {
 	player.pause();
-
-	// Unmute game
-	module.F('OBS.UnmuteAudioSource', 'Zoom HD60S');
 }
 
-async function musicStop(name, event) {
+async function stop(name, event) {
+	queue = [];
 	player.currentTime = 0;
 	player.pause();
-
-	// Unmute game
-	module.F('OBS.UnmuteAudioSource', 'Zoom HD60S');
 }
 
-async function musicShuffle(name, event) {
-	let shouldShuffle = (event === true);
-	
-	console.log("try changing shuffling from:", isShuffling, "to", shouldShuffle);
-	// If the setting is the same as it was, exit here
-	if(shouldShuffle === isShuffling)
+async function shuffle(name, event) {
+	queue = Utility.getArrayShuffled(items);
+	await tryPlayNextInQueue();
+}
+
+async function skip(name, event) {
+	await tryPlayNextInQueue();
+}
+
+async function queueSong(name, event) {
+	let item = await tryGetItem(event);
+	if (item === null) {
 		return;
+	}
 	
-	if(shouldShuffle) {
-		player.addEventListener('ended', playRandomTrack);
-		isShuffling = true;
-	} else {
-		player.removeEventListener('ended', playRandomTrack);
-		isShuffling = false;
+	queue.push(event);
+	
+	if(player.paused) {
+		await tryPlayNextInQueue();
 	}
 }
 
-async function playRandomTrack() {
-	let track = Utility.getRandomItem(items);
-	await musicPlay(null, track);
+async function queueAlbum(name, event) {
+	let albumItems = await tryGetAlbum(event);
+	if (albumItems === null) {
+		return;
+	}
+	
+	queue.push(...albumItems);
+	
+	if(player.paused) {
+		await tryPlayNextInQueue();
+	}
+}
+
+async function shuffleAlbum(name, event) {
+	let albumItems = await tryGetAlbum(event);
+	if (albumItems === null) {
+		return;
+	}
+	
+	queue = Utility.getArrayShuffled(albumItems);
+	
+	await tryPlayNextInQueue();
+}
+
+async function tryPlayNextInQueue() {
+	// Stop the current track
+	await player.pause();
+	player.currentTime	= 0;
+	player.src = null;
+	
+	// If we have no other songs, exit
+	if(queue.length === 0) {
+		module.F('Console.Log', 'The end of the music queue has been reached.');
+		return;
+	}
+	
+	// Try playing the next item in the queue
+	let itemName = queue.shift();
+	module.F('Console.Log', 'Now playing \"' + itemName + '\".');
+	let item = await tryGetItem(itemName);
+	
+	player.src = folderName + item;
+	await player.play();
 }
 
 async function logAllOptions(name, event) {
-	var regexGet = /\/(.+)\.[^.]+$/;
-	var trackNames = [];
-	var publicLog = '';
+	let regexGet = /\/(.+)\.[^.]+$/;
+	let trackNames = [];
+	let publicLog = '';
 
 	// Get clean names
-	for(var i = 0, l = items.length; i < l; i ++)
-	{
-		var displayName = regexGet.exec(items[i])[1];
+	for(let i = 0, l = items.length; i < l; i ++) {
+		let displayName = regexGet.exec(items[i])[1];
 		trackNames.push(displayName);
 	}
 
 	// Compile them all into one sorted log
 	trackNames.sort();
-	for(var i = 0, l = trackNames.length; i < l; i ++)
-	{
+	for(let i = 0, l = trackNames.length; i < l; i ++) {
 		if(i > 0)
 			publicLog += '|';
 
@@ -131,4 +196,4 @@ async function logAllOptions(name, event) {
 //// LISTENERS ////
 ///////////////////
 
-player.addEventListener('ended', () => musicStop(null, null));
+player.addEventListener('ended', tryPlayNextInQueue);
